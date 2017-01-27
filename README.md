3f6cb84706ae83e4cd169afb1d5b50148c1e49eb
Fried Hoeben <github@hsac.nl>
2017-01-27 17:45:21 +0100

    Create sample project using hsac-fitnesse-fixtures as baseline

---
+++ README.md
@@ -0,0 +1,5 @@
+Create your own FitNesse installation/testSuite based on [hsac-fitnesse-fixtures](https://github.com/fhoeben/hsac-fitnesse-fixtures)
+using Maven.
+
+
+To start the wiki `mvn clean compile exec:exec`, and access it at [http://localhost:9090/](http://localhost:9090/)
---
+++ src/test/java/FixtureDebugTest.java
@@ -0,0 +1,11 @@
+import fitnesse.junit.FitNesseRunner;
+import nl.hsac.fitnesse.junit.HsacFitNesseRunner;
+import org.junit.runner.RunWith;
+
+/**
+ * Test class to allow fixture code to be debugged.
+ */
+@RunWith(HsacFitNesseRunner.class)
+@FitNesseRunner.Suite("HsacAcceptanceTests.SlimTests.BrowserTest.ClickByLabelOrButton")
+public class FixtureDebugTest {
+}
