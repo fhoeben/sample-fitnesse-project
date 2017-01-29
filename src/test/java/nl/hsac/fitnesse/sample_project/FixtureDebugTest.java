6acbf744477e9a78c10d84b693e39fef87305cc7
Fried Hoeben <github@hsac.nl>
2017-01-29 11:13:20 +0100

    Version created by https://github.com/fhoeben/fitnesse-project-archetype

--- src/test/java/nl/hsac/fitnesse/sample_project/FixtureDebugTest.java
+++ src/test/java/nl/hsac/fitnesse/sample_project/FixtureDebugTest.java
@@ -1,9 +1,11 @@
+package nl.hsac.fitnesse.sample_project;
+
 import fitnesse.junit.FitNesseRunner;
 import nl.hsac.fitnesse.junit.HsacFitNesseRunner;
 import org.junit.runner.RunWith;
 
 /**
- * Test class to allow fixture code to be debugged.
+ * Test class to allow fixture code to be debugged, or run by build server.
  */
 @RunWith(HsacFitNesseRunner.class)
 @FitNesseRunner.Suite("HsacAcceptanceTests.SlimTests.UtilityFixtures.MapFixtureTest")
